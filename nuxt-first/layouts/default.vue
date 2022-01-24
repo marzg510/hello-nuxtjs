5e154750c4ecef6525b0cea29150daef9d758338
masaru goto <masaru.gotou@gmail.com>
2022-01-24 16:48:46 +0900

    navbar追加

---
+++ nuxt-first/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav-bar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+export default {
+  components:{
+    NavBar,
+  }
+}
+</script>
